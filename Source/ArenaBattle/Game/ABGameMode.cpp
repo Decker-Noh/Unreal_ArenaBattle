38a913c04a47be2ab34f89956bbd0e9e7b2eb035
Decker-Noh <76258933+Decker-Noh@users.noreply.github.com>
2024-07-01 06:29:12 +0900

    Arena : 레벨, 월드, 컨트롤러, 캐릭터 생성 및 설정해보기

---
+++ Source/ArenaBattle/Game/ABGameMode.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Game/ABGameMode.h"
+#include "Player/ABPlayerController.h"
+
+AABGameMode::AABGameMode()
+{
+
+	//GameController
+	PlayerControllerClass = AABPlayerController::StaticClass();
+	//PawnClass
+	static ConstructorHelpers::FClassFinder<APawn> ThirdPersonClassRef(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter.BP_ThirdPersonCharacter_C"));
+	if (ThirdPersonClassRef.Class != nullptr)
+	{
+		DefaultPawnClass = ThirdPersonClassRef.Class;
+	}
+}
