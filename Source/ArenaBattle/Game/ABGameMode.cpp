68d9c42047c1c7cbd13d9ee6208bd9ea82821787
Decker-Noh <76258933+Decker-Noh@users.noreply.github.com>
2024-07-01 10:47:25 +0900

    캐릭터, 이동

--- Source/ArenaBattle/Game/ABGameMode.cpp
+++ Source/ArenaBattle/Game/ABGameMode.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "Game/ABGameMode.h"
@@ -10,7 +10,13 @@ AABGameMode::AABGameMode()
 	//GameController
 	PlayerControllerClass = AABPlayerController::StaticClass();
 	//PawnClass
-	static ConstructorHelpers::FClassFinder<APawn> ThirdPersonClassRef(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter.BP_ThirdPersonCharacter_C"));
+	/*static ConstructorHelpers::FClassFinder<APawn> ThirdPersonClassRef(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter.BP_ThirdPersonCharacter_C"));
+	if (ThirdPersonClassRef.Class != nullptr)
+	{
+		DefaultPawnClass = ThirdPersonClassRef.Class;
+	}*/
+
+	static ConstructorHelpers::FClassFinder<APawn> ThirdPersonClassRef(TEXT("/Script/ArenaBattle.ABCharacterPlayer"));
 	if (ThirdPersonClassRef.Class != nullptr)
 	{
 		DefaultPawnClass = ThirdPersonClassRef.Class;
--- Source/ArenaBattle/Player/ABCharacterPlayer.h
+++ Source/ArenaBattle/Player/ABCharacterPlayer.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Charactor/ABCharacterBase.h"
+#include "InputActionValue.h"
 #include "ABCharacterPlayer.generated.h"
 
 /**
@@ -13,5 +14,35 @@ UCLASS()
 class ARENABATTLE_API AABCharacterPlayer : public AABCharacterBase
 {
 	GENERATED_BODY()
-	
+
+public:
+	AABCharacterPlayer();
+private:
+protected:
+
+	virtual void BeginPlay() override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	// Camera Section
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class USpringArmComponent> CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UCameraComponent> FollowCamera;
+
+	//Input System
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputMappingContext> DefaultMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputAction> JumpAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputAction> MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputAction> LookAction;
+
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
 };
